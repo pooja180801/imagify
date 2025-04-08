3830128fdaefc5e77eafe6d55f4daf2cc4cc853e
poojak <139669968+pooja180801@users.noreply.github.com>
2025-04-08 10:07:45 +0530

    added header step amd desc in home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import Header from '../components/Header'
+import Steps from '../components/Steps'
+import Description from '../components/Description'
 
 const Home = () => {
   return (
     <div>
-      Home
+        <Header/>
+        <Steps/>
+        <Description/>
     </div>
   )
 }
