5a5993c6eeb708735baa750cf96c9ecf9284c447
poojak <139669968+pooja180801@users.noreply.github.com>
2025-04-08 10:06:53 +0530

    added description.jsx

---
+++ client/src/components/Description.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+const Description = () => {
+  return (
+    <div className="flex flex-col text-center justify-center my-24 p-6 md:px-28">
+      <h1 className="text-3xl sm:text-4xl font-semibold mb-2">
+        Create AI Images
+      </h1>
+      <p className="text-gray-500 mb-8">Turn your imagination into visuals</p>
+
+      <div className="flex flex-col gap-5 md:gap-14 md:flex-row">
+        <img
+          src={assets.sample_img_1}
+          alt=""
+          className="w-80 xl:w-96 rounded-lg"
+        />
+        <div>
+          <h2 className="text-3xl font-medium max-w-lg mb-4">Introducing the AI-Powered Text to Image Generator</h2>
+          <p className="text-gray-600">
+            Bring your ideas to life with our AI-powered Text to Image
+            Generator. Just enter a description, and the AI creates stunning
+            visuals in seconds. It's fast, easy to use, and perfect for
+            creatives of all kinds. Turn your words into art effortlessly!
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Description;
