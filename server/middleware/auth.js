00ec3af55df4d56953c8169b8d527a94da01a734
poojak <139669968+pooja180801@users.noreply.github.com>
2025-04-15 17:57:29 +0530

    added auth.js to get the user id from jwt token

---
+++ server/middleware/auth.js
@@ -0,0 +1,25 @@
+import jwt from "jsonwebtoken";
+
+const userAuth = (req, res, next) => {
+    const {token} = req.headers;
+    if(!token){
+        return res.json({success: false, message: "Not Authorized. Please Login Again"});
+    }
+
+    try {
+        const tokenDecode = jwt.verify(token, process.env.JWT_SECRET);
+        console.log(tokenDecode)
+        if(tokenDecode.id){
+            req.body.userId = tokenDecode.id;
+        }else{
+            return res.json({success: false, message: "Not Authorized. Please Login Again"});
+        }
+
+        next();
+
+    } catch (error) {
+        res.json({success: false, message: error.message});
+    }
+}
+
+export default userAuth;
