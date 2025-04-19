c65fc973d164459e755177045c264a4e5280b0f7
poojak <139669968+pooja180801@users.noreply.github.com>
2025-04-19 17:03:35 +0530

    implemeted prompt entering result page

--- client/src/pages/Result.jsx
+++ client/src/pages/Result.jsx
@@ -1,11 +1,92 @@
-import React from 'react'
+import React, { useContext, useState } from "react";
+import { assets } from "../assets/assets";
+import { LinearProgress } from "@mui/material";
+import { motion } from "framer-motion";
+import { AppContext } from "../context/AppContext";
 
 const Result = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const [image, setImage] = useState(assets.sample_img_1);
+  const [isImageLoaded, setIsImageLoaded] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [input, setInput] = useState("");
+  const{generateImage} = useContext(AppContext)
 
-export default Result
+  const onSubmitHandler = async (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+
+    if(input){
+      const image = await generateImage(input)
+      console.log('image',image)
+      if(image){
+        setIsImageLoaded(true);
+        setImage(image);
+      }
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <motion.form
+      onSubmit={onSubmitHandler}
+      className="flex flex-col min-h-[90vh] items-center"
+      initial={{ opacity: 0.2, y: 100 }}
+      transition={{ duration: 1 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      viewport={{ once: true }}
+    >
+      <div>
+        <div className="relative">
+          <img src={`data:image/png;base64,${image}`}  alt="" className="max-w-70 rounded" />
+{/*           for premiem
+          <img src={image}  alt="" className="max-w-70 rounded" /> */}
+          {isLoading && (
+            <LinearProgress
+              color="primary"
+              sx={{ position: "absolute", bottom: 0, left: 0, width: "100%" }}
+            />
+          )}
+        </div>
+      </div>
+
+      {!isImageLoaded && (
+        <div className="flex w-full max-w-xl bg-neutral-500 text-white text-sm p-0.5 mt-10 rounded-full">
+          <input
+            onChange={(e) => setInput(e.target.value)}
+            value={input}
+            type="text"
+            placeholder="Describe what you want to generate"
+            className="flex-1 bg-transparent outline-none ml-8 max-sm:w-20 placeholder-color"
+          />
+          <button
+            type="submit"
+            className="bg-zinc-900 px-10 sm:px-16 py-3 rounded-full"
+          >
+            Generate
+          </button>
+        </div>
+      )}
+      {isImageLoaded && (
+        <div className="flex gap-2 flex-wrap justify-center text-white text-sm p-0.5 mt-10 rounded-full">
+          <p
+            onClick={() => {
+              setIsImageLoaded(false);
+            }}
+            className="bg-transparent border border-zinc-900 text-black px-8 py-3 rounded-full cursor-pointer"
+          >
+            Generate Another
+          </p>
+          <a
+            href={image}
+            download
+            className="bg-zinc-900 px-10 py-3 rounded-full cursor-pointer"
+          >
+            Download
+          </a>
+        </div>
+      )}
+    </motion.form>
+  );
+};
+
+export default Result;
