bf995c2f79e7fad4662417264a9a20f37a69f9ab
poojak <139669968+pooja180801@users.noreply.github.com>
2025-05-08 16:34:16 +0530

    Updated components

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,20 +1,90 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
+import { toast } from "react-toastify";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 export const AppContext = createContext();
 
 const AppContextProvider = (props) => {
-    const [user, setUser] = useState(null);
+  const [user, setUser] = useState(null);
+  const [showLogin, setShowLogin] = useState(false);
+  const [token, setToken] = useState(localStorage.getItem("token"));
+  const [credit, setCredit] = useState(false);
 
-    const value = {
-        user,
-        setUser,   
-    };
+  const navigate = useNavigate();
 
-    return (
-        <AppContext.Provider value={value}>
-            {props.children}
-        </AppContext.Provider>
-    );
+  const backendUrl = import.meta.env.VITE_BACKEND_URL;
+
+  const loadCreditData = async () => {
+    try {
+      const { data } = await axios.get(backendUrl + "/api/user/credits", {
+        headers: { token },
+      });
+
+      if (data.success) {
+        setCredit(data.credits);
+        setUser(data.user);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error(error.message);
     }
+  };
 
-    export default AppContextProvider;
+  const generateImage = async (prompt) => {
+    try {
+      const { data } = await axios.post(
+        backendUrl + "/api/image/generate-image",
+        { prompt },
+        { headers: { token } }
+      );
+      console.log('data',data)
+      if (data.success) {
+        loadCreditData();
+        return data.resultImage;
+      } else {
+        toast.error('Image generation failed. Please check your input or try again.');
+      }
+    } catch (error) {
+      toast.error('Image generation failed. Please check your input or try again.');
+      loadCreditData();
+      if(data.creditBalance === 0){
+        navigate('/buy')
+      }
+    }
+  };
+
+  const logout = () => {
+    localStorage.removeItem("token");
+    setToken("");
+    setUser(null);
+  };
+
+  useEffect(() => {
+    if (token) {
+      loadCreditData();
+      console.log("credit", credit);
+    }
+  }, [token]);
+
+  const value = {
+    user,
+    setUser,
+    showLogin,
+    setShowLogin,
+    backendUrl,
+    token,
+    setToken,
+    credit,
+    setCredit,
+    logout,
+    loadCreditData,
+    generateImage
+  };
+
+  return (
+    <AppContext.Provider value={value}>{props.children}</AppContext.Provider>
+  );
+};
+
+export default AppContextProvider;
