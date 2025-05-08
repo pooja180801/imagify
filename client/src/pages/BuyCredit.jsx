095d550a76ed296aec25ebf6a784319ff6e241cc
poojak <139669968+pooja180801@users.noreply.github.com>
2025-05-08 16:32:48 +0530

    Implemented Buy Credit landing page

--- client/src/pages/BuyCredit.jsx
+++ client/src/pages/BuyCredit.jsx
@@ -1,11 +1,66 @@
-import React from 'react'
+import React, { useContext } from "react";
+import { assets, plans } from "../assets/assets";
+import { AppContext } from "../context/AppContext";
+import { motion } from "framer-motion";
+import { useNavigate } from "react-router-dom";
+import axios from 'axios';
 
 const BuyCredit = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const { user, backendUrl, loadCreditData, token, setShowLogin } = useContext(AppContext);
+  const navigate = useNavigate();
 
-export default BuyCredit
+  const paymentStripe = async(planId) => {
+    try {
+      if(!user){
+        setShowLogin(true)
+      }
+
+      const {data} = await axios.post(backendUrl + '/api/user/pay-stripe', {planId}, {headers: {token}})
+      if(data.success){
+        const {session_url}=data
+        window.location.replace(session_url)
+      }
+    } catch (error) {
+      toast.error(error.message)
+    }
+  }
+
+  return (
+    <motion.div
+      className="min-h-[80vh] text-center pt-14 mb-10"
+      initial={{ opacity: 0.2, y: 100 }}
+      transition={{ duration: 1 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      viewport={{ once: true }}
+    >
+      <button className="border border-gray-400 px-10 py-2 rounded-full mb-6">
+        Our Plans
+      </button>
+      <h1 className="text-center text-3xl font-medium mb-6 sm:mb-10">
+        Choose the plan
+      </h1>
+
+      <div className="flex flex-wrap justify-center gap-6 text-left">
+        {plans.map((plan, index) => (
+          <div
+            key={index}
+            className="bg-white drop-shadow-sm border rounded-lg py-12 px-8 text-gray-600 hover:scale-105 transition-all duration-500"
+          >
+            <img src={assets.logo_icon} alt="" width={40} />
+            <p className="mt-3 mb-1 font-semibold">{plan.id}</p>
+            <p className="text-sm">{plan.desc}</p>
+            <p className="mt-6">
+              <span className="text-3xl font-medium">${plan.price}</span> /{" "}
+              {plan.credits} credits
+            </p>
+            <button onClick={()=>paymentStripe(plan.id)} className="w-full bg-gray-800 text-white mt-8 text-sm rounded-md py-2.5 min-w-52">
+              {user ? "Purchase" : "Get Started"}
+            </button>
+          </div>
+        ))}
+      </div>
+    </motion.div>
+  );
+};
+
+export default BuyCredit;
